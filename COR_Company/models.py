7f4d34e035262d12417f3af6c8a08a87a5802ad3
Luis Ovidio Gonzalez Alzate <logonzalez@enertotalesp.com>
2021-09-25 10:20:23 -0500

    first commit - creacion de apps y models

---
+++ COR_Company/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+
+class Company(models.Model):
+
+    OWNER_COMPANY = 1
+    CUSTOMER_COMPANY = 2
+
+    COMPANY_TYPE_CHOICES = (
+        (OWNER_COMPANY, 'PROPIETARIA'),
+        (CUSTOMER_COMPANY, 'CLIENTE')
+    )
+
+    id = models.AutoField(primary_key=True, verbose_name="ID")
+    owner = models.ForeignKey(User, verbose_name='Propietario', related_name='company_owner', on_delete=models.PROTECT, null=True, blank=True)
+    nit = models.IntegerField(verbose_name='NIT')
+    company_name = models.CharField(verbose_name='Nombre Empresa', max_length=200)
+    commercial_name = models.CharField(verbose_name='Nombre Comercial', max_length=200)
+    address = models.CharField(verbose_name='Direccion', max_length=200)
+    phone_number = models.CharField(verbose_name='Telefono', max_length=20)
+    email = models.EmailField(verbose_name='Correo Electronico', null=True, blank=True)
+    web_site = models.CharField(verbose_name='Sitio Web', null=True, blank=True, max_length=200)
+    country = models.CharField(verbose_name='Pais', max_length=100)
+    state = models.CharField(verbose_name='Estado/Departamento', max_length=100)
+    city = models.CharField(verbose_name='Ciudad', max_length=100)
+    company_type = models.IntegerField(verbose_name='Tipo Empresa', choices=COMPANY_TYPE_CHOICES)
+    father_company = models.ForeignKey('self', verbose_name='Empresa Padre', blank=True, null=True)
+    created_by = models.ForeignKey(User, db_column='created_by', related_name='company_created_by',
+                                   on_delete=models.PROTECT, verbose_name='Creado por')
+    created_date = models.DateField(auto_now=True, editable=False, verbose_name='Fecha Creacion')
+    updated_by = models.ForeignKey(User, blank=True, null=True, db_column='updated_by',
+                                   related_name='company_updated_by',
+                                   on_delete=models.PROTECT, verbose_name='Modificado por')
+    updated_date = models.DateField(blank=True, null=True, verbose_name='Fecha Modificacion')
+
+    def __str__(self):
+        return '%d' % self.id
+
+    class Meta:
+        managed = True
+        db_table = 'company'
+        verbose_name = 'Empresa'
+        verbose_name_plural = 'Empresas'
+
+
+class CompanyUsers(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="ID")
+    id_company = models.ForeignKey(Company, verbose_name='Empresa', on_delete=models.PROTECT)
+    id_user = models.ForeignKey(User, verbose_name='Usuario', on_delete=models.PROTECT)
+    created_date = models.DateField(auto_now=True, editable=False, verbose_name='Fecha Creacion')
+    updated_date = models.DateField(blank=True, null=True, verbose_name='Fecha Modificacion')
+
+    def __str__(self):
+        return '%d' % self.id
+
+    class Meta:
+        managed = True
+        db_table = 'company_users'
+        verbose_name = 'Usuario Invitado'
+        verbose_name_plural = 'Usuarios Invitados'
---
+++ COR_Customer/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+from COR_Company.models import Company
+
+
+class Contact(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="UUID")
+    first_name = models.CharField(verbose_name='Primer nombre', max_length=100)
+    middle_name = models.CharField(verbose_name='Segundo Nombre', blank=True, null=True, max_length=100)
+    last_name = models.CharField(verbose_name='Apellidos', max_length=200)
+    email = models.EmailField(verbose_name='Correo Electronico', null=True, blank=True)
+    phone_number = models.CharField(verbose_name='Telefono', max_length=20)
+    mobile_phone_number = models.CharField(verbose_name='Celular', null=True, blank=True, max_length=20)
+    id_company = models.ForeignKey(Company, verbose_name='ID Empresa')
+    created_by = models.ForeignKey(User, db_column='created_by', related_name='contact_created_by',
+                                   on_delete=models.PROTECT, verbose_name='Creado por')
+    created_date = models.DateField(auto_now=True, editable=False, verbose_name='Fecha Creacion')
+    updated_by = models.ForeignKey(User, blank=True, null=True, db_column='updated_by',
+                                   related_name='contact_updated_by',
+                                   on_delete=models.PROTECT, verbose_name='Modificado por')
+    updated_date = models.DateField(blank=True, null=True, verbose_name='Fecha Modificacion')
+
+    def __str__(self):
+        return '%d' % self.id
+
+    class Meta:
+        managed = True
+        db_table = 'contact'
+        verbose_name = 'Contacto'
+        verbose_name_plural = 'Contactos'
+
+
+class BusinessOpportunity(models.Model):
+    PROCESS_STATUS = 'P'
+    ACCEPTED_STATUS = 'W'
+    NO_ACCEPTED_STATUS = 'NW'
+    CANCEL_STATUS = 'D'
+
+    OPPORTUNITY_CHOICES = (
+        (PROCESS_STATUS, 'EN PROCESO'),
+        (ACCEPTED_STATUS, 'GANADA'),
+        (NO_ACCEPTED_STATUS, 'NO GANADA'),
+        (CANCEL_STATUS, 'CANCELADA')
+    )
+
+    id = models.AutoField(primary_key=True, verbose_name="UUID")
+    id_company = models.ForeignKey(Company, verbose_name='Empresa')
+    id_contact = models.ForeignKey(Contact, verbose_name='Contacto')
+    opportunity_name = models.CharField(verbose_name='Nombre', max_length=100)
+    opportunity_value = models.IntegerField(verbose_name='Monto')
+    status = models.CharField(verbose_name='Estado', choices=OPPORTUNITY_CHOICES, max_length=5)
+    created_by = models.ForeignKey(User, db_column='created_by', related_name='opportunity_created_by',
+                                   on_delete=models.PROTECT, verbose_name='Creado por')
+    created_date = models.DateField(auto_now=True, editable=False, verbose_name='Fecha Creacion')
+    updated_by = models.ForeignKey(User, blank=True, null=True, db_column='updated_by',
+                                   related_name='opportunity_updated_by',
+                                   on_delete=models.PROTECT, verbose_name='Modificado por')
+    updated_date = models.DateField(blank=True, null=True, verbose_name='Fecha Modificacion')
+
+    def __str__(self):
+        return '%d' % self.id
+
+    class Meta:
+        managed = True
+        db_table = 'business_opportunity'
+        verbose_name = 'Oportunidad de Negocio'
+        verbose_name_plural = 'Oportunidades de Negocio'
